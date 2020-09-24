c044ed4899a7dc81a15eee4da8e40caca298b52a
Eric Coughlin <eric.coughlin2014@gmail.com>
2020-09-24 13:35:35 -0500

    Added the shared library resources for the CSC Conda packaging jobs.
    Update conda_package_builder to pull ts-dockerhub and add dev conda package stage
    
    Add conda variant support to conda Jenkinsfile
    
    Print out salobj and idl parameter versions
    
    Add label to conda build to find idl and salobj packages
    
    Added default values for the idl_version and salobj_version parameters. Added the label argument to the build_conda function and updated the calls to the function to set the value.
    
    Set the RC regexp to look for any character before the 'rc' string, instead of specifically a dash.

--- README.md
+++ README.md
@@ -1,34 +1,36 @@
 # Jenkins Shared Library
 
-This contains the shared libaries for the Telescope and Site Software Team's Jenkins instance.
+This contains the shared libraries for the Telescope and Site Software Team's Jenkins instance.
 
 According to the shared library documentation, pretty much any groovy object is allowed.
 However, the caveat here is that classes are not allowed to use the built-in DSL calls like `sh`.
-So we workaround it by making a file which defines functions and then at the end returing `this` which allows it to behave like an object.
+So we workaround it by making a file which defines functions and then at the end returning `this` which allows it to behave similar to a class without the object reflection.
 `Csc.groovy` contains a set of functions to help with the lifecycle for a CSC build.
-The `pipelines` contain the different types of pipelines for the CSC lifecycle.
+The `vars` folder contain the different types of **declarative** pipelines for the CSC lifecycle.
 
 An example is below of what a `Jenkinsfile` can look like.
 
 ```Jenkinsfile
-import com.lsst.ts.jenkins.*
+@Library('SharedLibrary') _
 
-Pipelines pipelines = new Pipelines()
-
-pipelines.condaPipeline("ts_config_attcs", "ts-athexapod", "lsst.ts.ATHexapod")
+CondaPipeline("ts_config_attcs", "ts-athexapod", "lsst.ts.ATHexapod")
 
 ```
+
 *Components*
+
 Contains logic for abstracting tools used in building software.
 
-*pipelines*
+*vars*
+
 Contains logic for running different pipelines.
 
-The hierarchy and patterns are taken from [Chris Cooney's Repo.](https://github.com/ChrisCooney/jenkins-global-library-simple)
+The hierarchy and patterns are taken from [Chris Cooney's Repo](https://github.com/ChrisCooney/jenkins-global-library-simple). Also taken from [gfkse's repo](https://github.com/gfkse/jenkins-shared-library)
 
 
 ## References
-https://github.com/ChrisCooney/jenkins-global-library-simple
-https://www.jenkins.io/doc/book/pipeline/shared-libraries/
-https://groovy-lang.org/syntax.html
-https://www.jenkins.io/doc/book/pipeline/pipeline-best-practices/#using-shared-libraries
+* https://github.com/ChrisCooney/jenkins-global-library-simple
+* https://www.jenkins.io/doc/book/pipeline/shared-libraries/
+* https://groovy-lang.org/syntax.html
+* https://www.jenkins.io/doc/book/pipeline/pipeline-best-practices/#using-shared-libraries
+* https://github.com/gfkse/jenkins-shared-library
---
+++ vars/CondaPipeline.groovy
@@ -0,0 +1,121 @@
+import org.lsst.ts.jenkins.components.Csc
+
+def call(config_repo, name, module_name){
+    // Create a conda build pipeline
+    Csc csc = new Csc()
+    pipeline {
+        agent {
+            docker {
+                image 'ts-dockerhub.lsst.org/conda_package_builder:latest'
+                alwaysPull true
+                label 'CSC_Conda_Node'
+                args "--env ${config_repo.toUpperCase()}_DIR=/home/saluser/${config_repo} --env LSST_DDS_DOMAIN=citest -u root --entrypoint=''"
+                registryUrl 'https://ts-dockerhub.lsst.org'
+                registryCredentialsId 'nexus3-lsst_jenkins'
+            }
+        }
+        parameters {
+            string(name: 'idl_version', defaultValue: '\'\'', description: 'The version of the IDL Conda package.')
+            string(name: 'salobj_version', defaultValue: '\'\'', description: 'The version of the salobj Conda package.')
+        }
+        environment {
+            package_name = "${name}"
+            OSPL_HOME="/opt/OpenSpliceDDS/V6.10.4/HDE/x86_64.linux"
+        }
+        stages {
+            stage("Clone configuration repository") {
+                steps {
+                    sh """
+                        echo "The IDL version: ${params.idl_version}"
+                        echo "The SalObj version: ${params.salobj_version}"
+                        cd /home/saluser
+                        git clone https://github.com/lsst-ts/${config_repo}
+                    """
+                }
+            }
+            stage("Create Conda Package") {
+                when {
+                    buildingTag()
+                }
+                steps {
+                    withEnv(["HOME=${env.WORKSPACE}"]) {
+                        script {
+                            csc.build_conda("main")
+                        }
+                    }
+                }
+            }
+            stage("Create Conda dev package") {
+                when {
+                    not {
+                        buildingTag()
+                    }
+                }
+                steps {
+                    withEnv(["HOME=${env.WORKSPACE}"]) {
+                        script {
+                            csc.build_conda("dev")
+                        }
+                    }
+                }
+            }
+            stage("Push Conda Release Candidate package") {
+                when {
+                    buildingTag()
+                    tag pattern: "^v\\d\\.\\d\\.\\d\\.rc\\.\\d\$", comparator: "REGEXP"
+                }
+                steps {
+                    withCredentials([usernamePassword(credentialsId: 'CondaForge', passwordVariable: 'anaconda_pass', usernameVariable: 'anaconda_user')]) {
+                        withEnv(["HOME=${env.WORKSPACE}"]) {
+                            sh """
+                            source /home/saluser/miniconda3/bin/activate
+                            anaconda login --user ${anaconda_user} --password ${anaconda_pass}
+                            """
+                            script {
+                                csc.upload_conda(package_name,"rc")
+                            }
+                        }
+                    }
+                }
+            }
+            stage("Push Conda Release package") {
+                when {
+                    buildingTag()
+                    not {
+                        tag pattern: "^v\\d\\.\\d\\.\\d\\.rc\\.\\d\$", comparator: "REGEXP"
+                    }
+                }
+                steps {
+                    withCredentials([usernamePassword(credentialsId: 'CondaForge', passwordVariable: 'anaconda_pass', usernameVariable: 'anaconda_user')]) {
+                        withEnv(["HOME=${env.WORKSPACE}"]) {
+                            sh """
+                            source /home/saluser/miniconda3/bin/activate
+                            anaconda login --user ${anaconda_user} --password ${anaconda_pass}
+                            """
+                            script {
+                                csc.upload_conda(package_name,"main")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                withEnv(["HOME=${env.WORKSPACE}"]) {
+                    sh 'chown -R 1003:1003 ${HOME}/'
+                }
+            }
+            regression {
+                emailext body: 'Check console output at $BUILD_URL to view the results. \n\n ${CHANGES} \n\n -------------------------------------------------- \n${BUILD_LOG, maxLines=100, escapeHtml=false}', 
+                    recipientProviders: [culprits()], 
+                    subject: 'Build failed in Jenkins: $PROJECT_NAME - #$BUILD_NUMBER'
+            }
+            fixed {
+            emailext body: 'Check console output at $BUILD_URL to view the results. \n\n ${CHANGES} \n', 
+                    recipientProviders: [culprits()], 
+                    subject: 'Build Fixed in Jenkins: $PROJECT_NAME - #$BUILD_NUMBER'
+            }
+        }
+    }
+}
