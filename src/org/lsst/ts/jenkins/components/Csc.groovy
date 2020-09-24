7eb155e218705bc35898a873651cdf0558b030b7
Eric Coughlin <eric.coughlin2014@gmail.com>
2020-09-24 13:35:35 -0500

    Added the shared library resources for the CSC Conda packaging jobs.
    Update conda_package_builder to pull ts-dockerhub and add dev conda package stage
    
    Add conda variant support to conda Jenkinsfile
    
    Print out salobj and idl parameter versions
    
    Add label to conda build to find idl and salobj packages
    
    Added default values for the idl_version and salobj_version parameters. Added the label argument to the build_conda function and updated the calls to the function to set the value.
    
    Set the RC regexp to look for any character before the 'rc' string, instead of specifically a dash.

---
+++ src/org/lsst/ts/jenkins/components/Csc.groovy
@@ -0,0 +1,45 @@
+package org.lsst.ts.jenkins.components
+
+def build_docs() {
+    // Build the documentation
+    sh "package-docs build"
+}
+
+def upload_docs(name) {
+    // upload the documentation
+    // Takes the product name as an argument
+    sh "ltd upload --product ${name} --git-ref ${GIT_BRANCH} --dir doc/_build/html"
+}
+
+def install() {
+    // Install the development requirements
+    sh "pip install .[dev]"
+}
+
+def test() {
+    // Run the tests
+    sh "pytest --cov-report html --cov=${env.MODULE_NAME} --junitxml=${env.XML_REPORT}"
+}
+
+def build_conda(label) {
+    // Build the conda package
+    sh """
+        cd ${HOME}/conda
+        source /home/saluser/miniconda3/bin/activate
+        conda config --add channels conda-forge
+        conda config --add channels lsstts
+        source ${OSPL_HOME}/release.com
+        conda build -c lsstts/label/${label} --variants "{salobj_version: ${params.salobj_version}, idl_version: ${params.idl_version}}" --prefix-length 100 .
+    """
+}
+
+def upload_conda(name, label) {
+    // Upload the conda package
+    // Takes the name of the package and a label
+    sh """
+        source /home/saluser/miniconda3/bin/activate
+        anaconda upload -u lsstts --label ${label} --force /home/saluser/miniconda3/conda-bld/linux-64/${name}*.tar.bz2
+    """
+}
+
+return this
