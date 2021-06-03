0ad3684d4f15d3858f3758770b834e68b0c362cc
Tiago Ribeiro <tiago.astro@gmail.com>
2021-06-03 10:55:40 -0700

    Add ExternalCondaPipeline, to build packages with recipes in ts_recipes.

---
+++ vars/ExternalCondaPipeline.groovy
@@ -0,0 +1,156 @@
+import org.lsst.ts.jenkins.components.Csc
+
+def call(name, arch="linux-64"){
+    // Create a conda build pipeline
+    Csc csc = new Csc()
+    emails = csc.email()
+    slack_ids = csc.slack_id()
+    arg_str = ""
+    clone_str = ""
+    if (arch=="linux-aarch64") {
+        label_value = "Arm64_2CPU"
+        image_value = "lsstts/conda_package_builder_aarch64:latest"
+        registry_url = ""
+        registry_credentials_id = ""
+    }
+    else {
+        label_value = "CSC_Conda_Node"
+        image_value = "ts-dockerhub.lsst.org/conda_package_builder:latest"
+        registry_url = "https://ts-dockerhub.lsst.org"
+        registry_credentials_id = "nexus3-lsst_jenkins"
+    }
+    properties(
+        [
+        buildDiscarder
+            (logRotator (
+                artifactDaysToKeepStr: '',
+                artifactNumToKeepStr: '',
+                daysToKeepStr: '14',
+                numToKeepStr: '10'
+            ) ),
+        disableConcurrentBuilds()
+        ]
+    )
+    pipeline {
+
+        agent {
+            docker {
+                image image_value
+                alwaysPull true
+                label label_value
+                args arg_str.concat("-u root --entrypoint=''")
+                registryUrl registry_url
+                registryCredentialsId registry_credentials_id
+            }
+        }
+
+        environment {
+            package_name = "${name}"
+            branch = csc.getBranchName(env.GIT_BRANCH, env.CHANGE_BRANCH)
+        }
+
+        options {
+          disableConcurrentBuilds()
+          skipDefaultCheckout()
+        }
+
+        stages {
+
+            stage ('Cloning Recipe Repos') {
+                steps {
+                    dir(env.WORKSPACE + '/ts_recipes') {
+                        git branch: 'master', url: 'https://github.com/lsst-ts/ts_recipes'
+                    }
+                }
+            }
+
+            stage("Create Conda Package") {
+                when {
+                    buildingTag()
+                }
+                steps {
+                    withEnv(["WHOME=${env.WORKSPACE}/ts_recipes/${package_name}", "CONDA_BUILD_TAG=${tag}"]) {
+                        script {
+                            csc.build_csc_conda("main")
+                        }
+                    }
+                }
+            }
+            stage("Create Conda dev package") {
+                when {
+                    not {
+                        buildingTag()
+                    }
+                }
+                steps {
+                    withEnv(["WHOME=${env.WORKSPACE}/ts_recipes/${package_name}", "CONDA_BUILD_TAG=${env.branch}"]) {
+                        script {
+                            csc.build_csc_conda("dev")
+                        }
+                    }
+                }
+            }
+            stage("Push Conda Release Candidate package") {
+                when {
+                    buildingTag()
+                    tag pattern: "^v\\d\\.\\d\\.\\d\\.rc\\.\\d\$", comparator: "REGEXP"
+                }
+                steps {
+                    withCredentials([usernamePassword(credentialsId: 'CondaForge', passwordVariable: 'anaconda_pass', usernameVariable: 'anaconda_user')]) {
+                        withEnv(["WHOME=${env.WORKSPACE}"]) {
+                            sh """
+                            source /home/saluser/miniconda3/bin/activate
+                            anaconda login --user ${anaconda_user} --password ${anaconda_pass}
+                            """
+                            script {
+                                csc.upload_conda(package_name,"rc", arch)
+                            }
+                        }
+                    }
+                }
+            }
+            stage("Push Conda Release package") {
+                when {
+                    buildingTag()
+                    not {
+                        tag pattern: "^v\\d\\.\\d\\.\\d\\.rc\\.\\d\$", comparator: "REGEXP"
+                    }
+                }
+                steps {
+                    withCredentials([usernamePassword(credentialsId: 'CondaForge', passwordVariable: 'anaconda_pass', usernameVariable: 'anaconda_user')]) {
+                        withEnv(["WHOME=${env.WORKSPACE}"]) {
+                            sh """
+                            source /home/saluser/miniconda3/bin/activate
+                            anaconda login --user ${anaconda_user} --password ${anaconda_pass}
+                            """
+                            script {
+                                csc.upload_conda(package_name,"main",arch)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                withEnv(["HOME=${env.WORKSPACE}"]) {
+                    sh 'chown -R 1003:1003 ${HOME}/'
+                }
+                step([$class: 'Mailer', recipients: emails[name], notifyEveryUnstableBuild: false, sendToIndividuals: true])
+            }
+            regression {
+                script {
+                    def userId = slack_ids[name]
+                    slackSend(color: "danger", message: "<@$userId> ${JOB_NAME} has suffered a regression ${BUILD_URL}", channel: "#jenkins-builds, @$userId")
+                }
+
+            }
+            fixed {
+                script {
+                    def userId = slack_ids[name]
+                    slackSend(color: "good", message: "<@$userId> ${JOB_NAME} has been fixed ${BUILD_URL}", channel: "#jenkins-builds, @$userId")
+                }
+            }
+        }
+    }
+}
