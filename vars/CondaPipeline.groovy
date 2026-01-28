852bbe8d8ca8730c48488c7d23399a8b002e4dd7
Eric Coughlin <ecoughlin@lsst.org>
2026-01-28 10:50:45 -0600

    Add legacy interface login EV to conda pipeline to fix auth workflow.

--- vars/CondaPipeline.groovy
+++ vars/CondaPipeline.groovy
@@ -206,6 +206,7 @@ def call(Object... varargs){
                         withEnv(["WHOME=${env.WORKSPACE}"]) {
                             sh """
                             source /home/saluser/miniconda3/bin/activate
+                            export ANACONDA_CLIENT_LEGACY_INTERACTIVE_LOGIN=1
                             anaconda login --user ${anaconda_user} --password ${anaconda_pass}
                             """
                             script {
