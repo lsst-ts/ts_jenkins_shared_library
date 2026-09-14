0fce320c88591f3e9d9a5ee9ea9472000422d36b
rbovill <rbovill@lsst.org>
2026-09-14 09:53:44 -0700

    Redefine the 'develop' parameter to the 'Bleed' parameter, instead, in XmlPipeline.groovy.

--- vars/XmlPipeline.groovy
+++ vars/XmlPipeline.groovy
@@ -43,7 +43,7 @@ def call(Object... varargs){
             choice choices: ['CSC_Conda_Node || CSC_Conda_Overflow_Node', 'Node1_4CPU', 'Node2_8CPU', 'Node3_4CPU'], description: 'Select the build agent', name: 'build_agent'
             string(name: 'XML_Version', description: 'The XML Version, exclude any preceeding "v" characters: X.Y.Z')
             string(name: 'SAL_Version', description: 'The SAL version, exclude any preceeding "v" characters: X.Y.Z')
-            booleanParam(description: "Is this a development build?", name: 'develop')
+            booleanParam defaultValue: params.Bleed ?:false, description: 'Is this a "bleeding edge" build?', name: 'Bleed'
             booleanParam(description: "Are we building the salobj conda package after this?", name: 'buildSalObjConda')
             booleanParam(description: "Are we going on to building the CSC package after salobj?", name: 'buildCSCConda')
         }
