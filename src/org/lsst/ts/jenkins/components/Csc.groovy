d835618f8c5bd3b9fff7dbfc6d483860363f7456
rbovill <rbovill@lsst.org>
2026-09-16 10:19:17 -0700

    actual fix to build salobj.

--- src/org/lsst/ts/jenkins/components/Csc.groovy
+++ src/org/lsst/ts/jenkins/components/Csc.groovy
@@ -312,7 +312,7 @@ def build_salobj_conda(label, concatVersion, pyver) {
             conda config --add channels conda-forge
             conda config --add channels lsstts
             conda config --add channels https://${nexus_user}:${nexus_pass}@repo-nexus.lsst.org/nexus/repository/ssw-conda/
-            conda build --python ${pyver} -c lsstts/label/${label} --variants "{xml_version: ${params.xml_conda_version}}" --prefix-length 100 .
+            conda build --python ${pyver} -c https://repo-nexus.lsst.org/nexus/repository/ssw-conda/ -c lsstts/label/${label} --variants "{xml_version: ${params.xml_conda_version}}" --prefix-length 100 .
         """
     }
 }
