4fdbf11e5b2f75127c95b1631da61c68acb657e5
rbovill <rbovill@gmail.com>
2026-09-17 12:11:54 -0700

    creds

--- src/org/lsst/ts/jenkins/components/Csc.groovy
+++ src/org/lsst/ts/jenkins/components/Csc.groovy
@@ -284,8 +284,7 @@ def build_standalone_conda(label, pyver) {
             conda config --set solver libmamba
             conda config --add channels conda-forge
             conda config --add channels lsstts
-            conda config --add channels https://${nexus_user}:${nexus_pass}@repo-nexus.lsst.org/nexus/repository/ssw-conda/
-            conda build --python ${pyver} -c https://repo-nexus.lsst.org/nexus/repository/ssw-conda/${label} -c lsstts/label/${label} --prefix-length 100 .
+            conda build --python ${pyver} -c https://${nexus_user}:${nexus_pass}@repo-nexus.lsst.org/nexus/repository/ssw-conda/${label} -c lsstts/label/${label} --prefix-length 100 .
         """
     }
 }
