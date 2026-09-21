5383e565706399dd8616a91f75fd0a8077654e37
rbovill <rbovill@lsst.org>
2026-09-21 13:03:49 -0700

    Fix23

--- src/org/lsst/ts/jenkins/components/Csc.groovy
+++ src/org/lsst/ts/jenkins/components/Csc.groovy
@@ -313,7 +313,7 @@ def build_salobj_conda(label, concatVersion, pyver) {
             conda config --set solver libmamba
             conda config --add channels conda-forge
             conda config --add channels lsstts
-            conda build --python ${pyver} -c https://${nexus_user}:${nexus_pass}@repo-nexus.lsst.org/nexus/repository/ssw-conda/dev/noarch/ --variants "{xml_version: ${params.xml_conda_version}}" --prefix-length 100 .
+            conda build --python ${pyver} -c https://${nexus_user}:${nexus_pass}@repo-nexus.lsst.org/nexus/repository/ssw-conda/dev --variants "{xml_version: ${params.xml_conda_version}}" --prefix-length 100 .
         """
     }
 }
